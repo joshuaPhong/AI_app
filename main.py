87cabce1b5fb8e8a0f4115ccba8cd1d9896b7030
joshuaPhong <klx2044@yahoo.com>
2023-05-15 12:19:12 +1200

    .gitignore

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
+# os provides acces to operating system
 import os
+# openai library
 import openai
+# dotenv is a library used for handling environment variables in a .env file
+from dotenv import load_dotenv
+load_dotenv()
 
-os.environ['OPENAI_API_KEY'] = "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"
+openai.api_key = os.getenv("API_KEY")
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+while True:
+    question = input("\033[31mWhat is your question? \n")
 
-question = input("\033[31mWhat is your question? \n")
-
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are an assistant. Answer thew "
-                                      "question"},
-        {"role": "user", "content": question}
-    ]
-)
-print(completion.choices[0].message.content)
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are an assistant. Answer thew "
+                                          "question"},
+            {"role": "user", "content": question}
+        ]
+    )
+    print("\033[32m" + completion.choices[0].message.content + '\n')
